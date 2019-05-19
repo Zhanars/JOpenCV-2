943cec01991f6bfa313267b3ff450fcab1cae7fe
Joker <a.janars@gmail.com>
2019-05-19 22:31:15 +0600

    first

--- src/main/java/PageMaker.java
+++ src/main/java/PageMaker.java
@@ -11,26 +11,29 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 
 public class PageMaker {
+    public static Mat src = new Mat();
     public static void Make(){
         String imgSrc = "images/source.png";
-        Mat src = Imgcodecs.imread(imgSrc);
-        for (int i = 0; i < 30; i++) {
+        src = Imgcodecs.imread(imgSrc);
+        int depth = 15;
+        for (int i = 0; i < depth; i++) {
             // 1 квадрат
-            Imgproc.rectangle(src, new Point(i, i), new Point(30, 30), new Scalar(0, 0));
+            Imgproc.rectangle(src, new Point(i, i), new Point(depth, depth), new Scalar(0, 0));
             // 2 квадрат
-            Imgproc.rectangle(src, new Point(src.width() - 30 + i, i), new Point(src.width(), 30), new Scalar(0, 0));
+            Imgproc.rectangle(src, new Point(src.width() - depth + i, i), new Point(src.width(), depth), new Scalar(0, 0));
             // 3 квадрат
-            Imgproc.rectangle(src, new Point(src.width()/2 - 15 + i, i), new Point(src.width()/2 + 15, 30), new Scalar(0, 0));
+            Imgproc.rectangle(src, new Point((src.width() - depth)/2 + i, i), new Point((src.width() + depth)/2, depth), new Scalar(0, 0));
             // 4 квадрат
-            Imgproc.rectangle(src, new Point(i,src.height() - 30 + i), new Point( 30, src.height()), new Scalar(0, 0));
+            Imgproc.rectangle(src, new Point(i,src.height() - depth + i), new Point( depth, src.height()), new Scalar(0, 0));
             // 5 квадрат
-            Imgproc.rectangle(src, new Point(src.width() - 30 + i, src.height() - 30 + i), new Point(src.width(), src.height()), new Scalar(0, 0));
+            Imgproc.rectangle(src, new Point(src.width() - depth + i, src.height() - depth + i), new Point(src.width(), src.height()), new Scalar(0, 0));
         }
+        predmet1(30, 8, 0);
         Imgproc.circle(src, new Point(415,545), 50, new Scalar(0, 0, 255));
         for (int i = 0; i < 3; i++) {
             System.out.println(src.get(100, 100)[i]);
         }
-        Imgcodecs.imwrite("images/out.png", src);
+        Imgcodecs.imwrite("images/out1.png", src);
         System.out.println("File ready");
     }
 
@@ -46,6 +49,37 @@ public class PageMaker {
         final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
         System.arraycopy(b, 0, targetPixels, 0, b.length);
         return image;
+    }
+    public static void predmet1(int countAnswer, int countVariant, int startX){
+        int depth = 2;
+        int Pixels = 38;
+        int startY = 1155;
+        int endY = startY + (countAnswer + 1) * Pixels + depth * 3;
+        int endX = startX + (countVariant + 1) * Pixels + depth * 3;
+        for (int i = 0; i < depth * 2; i++) {
+            Imgproc.rectangle(src, new Point(startX, startY), new Point(endX, endY), new Scalar(0, 0));
+            startX ++;
+            startY ++;
+            endX --;
+            endY --;
+        }
+        int newStartY = startY;
+        for (int i = 0; i < countAnswer; i++) {
+            System.out.println(startX + " " + newStartY + " " + endX + " " + (newStartY + 1));
+            newStartY += (Pixels - depth);
+            Imgproc.rectangle(src, new Point(startX, newStartY), new Point(endX, newStartY + 1), new Scalar(0, 0));
+            newStartY += depth;
+        }
+        int newStartX = startX;
+        startY += (Pixels - depth);
+        for (int i = 0; i < countVariant; i++) {
+            newStartX += (Pixels - depth);
+            Imgproc.rectangle(src, new Point(newStartX, startY), new Point(newStartX + 1, endY), new Scalar(0, 0));
+            newStartX += depth;
+        }
+
+
+
     }
 
 
