1b7475bf1716ab3b8beca6f3a860148ad61ae560
Joker <a.janars@gmail.com>
2019-06-17 21:46:40 +0600

    first

--- src/main/java/api.java
+++ src/main/java/api.java
@@ -8,19 +8,23 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import java.awt.*;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class api {
     private static String result = "";
 
     public synchronized static void inserBlank(String str) {
         HttpClient httpClient = HttpClientBuilder.create().build();
-        HttpPost request = new HttpPost("http://testreader.atu.kz/api/blanks.php");
         StringEntity params = null;
         try {
-            params = new StringEntity("{\"text\":\"" + str + "\"} ");
-            request.addHeader("content-type", "application/x-www-form-urlencoded");
-            request.setEntity(params);
+            HttpPost request = new HttpPost("https://testreader.atu.kz/api/blanks.php?text=" + URLEncoder.encode(str, "UTF-8"));
+            //params = new StringEntity("{\"text\":\"" + str + "\"} ");
+            //params.setContentType("application/json;charset=UTF-8");
+            //request.setEntity(params);
             HttpResponse response = httpClient.execute(request);
+            System.out.println(params);
+            System.out.println(request);
+            System.out.println(response.getStatusLine());
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         } catch (ClientProtocolException e) {
